dd01cc1fcead4e61fdfc693cdf5a8eb4460a6219
Clément Praud <clement.praud6@gmail.com>
2025-01-03 17:04:26 +0100

    Fill the Search Page component to show static version of the futur page

--- client/src/pages/SearchPage.tsx
+++ client/src/pages/SearchPage.tsx
@@ -1,5 +1,70 @@
+import searchIcon from "/search.svg";
+import "./search-page.css";
+import exempleNursery from "/aurelie_faugere_assistante_maternelle.svg";
+import funnelIcon from "/funnel.svg";
+
 function SearchPage() {
-  return <h1>Ici c'est la search page</h1>;
+  return (
+    <>
+      <header className="head-section">
+        {/* Should be replace by FilterBar component in the futur*/}
+        <section className="filter-head-section">
+          <img src={searchIcon} alt="magnifying glass" />
+          <input type="text" />
+        </section>
+        <section className="options-head-section">
+          <section className="filter-and-sort-options">
+            {/* Both figures above should triger a modal to fill filter or sort criteria*/}
+            <figure className="options">
+              <img src={funnelIcon} alt="funnel" />
+              <figcaption>Filtrer</figcaption>
+            </figure>
+            <figure className="options">
+              <img src={funnelIcon} alt="funnel" />
+              <figcaption>Trier</figcaption>
+            </figure>
+          </section>
+          <p>Carte</p>
+        </section>
+      </header>
+
+      <main>
+        <article className="nursery-section">
+          <figure>
+            <img src={exempleNursery} alt="example of the nursery" />
+            <figcaption>
+              Aurélie Faugère
+              <br />
+              Assistante maternelle
+            </figcaption>
+            <p>❤️</p>
+            <p>4.8</p>
+          </figure>
+          <h3>Bordeaux, à 500m</h3>
+          <p>Horaires : Lundi - Samedi : 9h-16h</p>
+          <p>Téléphone : 05 56 56 56 56</p>
+          <p>Mail : contact@contact.fr</p>
+        </article>
+
+        <article className="nursery-section">
+          <figure>
+            <img src={exempleNursery} alt="example of the nursery" />
+            <figcaption>
+              Crèche Picoti Picota
+              <br />
+              Crèche parentale
+            </figcaption>
+            <p>❤️</p>
+            <p>4.8</p>
+          </figure>
+          <h3>Bordeaux, à 500m</h3>
+          <p>Horaires : Lundi - Samedi : 9h-16h</p>
+          <p>Téléphone : 05 56 56 56 56</p>
+          <p>Mail : contact@contact.fr</p>
+        </article>
+      </main>
+    </>
+  );
 }
 
 export default SearchPage;
