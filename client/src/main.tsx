1f4b7d2b68cdb39964be61bd1e0bb520f318b69d
alicepgrd <pougeardalice@gmail.com>
2025-01-03 16:58:28 +0100

    globalscss done

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,8 +2,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
- 
-
 
 /* ************************************************************************* */
 
--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -1,6 +1,5 @@
 function LoginPage() {
   return <h1>Ici c'est la login page</h1>;
- 
 }
 
 export default LoginPage;
