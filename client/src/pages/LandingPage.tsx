f6717a8de1406ed80bd88186484d51674071f8d1
Marcellin123 <marcellindikelel@yahoo.fr>
2025-01-07 16:53:15 +0100

    landingPage-available

--- client/src/pages/LandingPage.tsx
+++ client/src/pages/LandingPage.tsx
@@ -1,10 +1,31 @@
+import logo from "/logo.svg";
+import mother from "/mother.svg";
+import "./Landingpage.css";
 function LandingPage() {
   return (
     <div className="landing-page">
-      <figure>
-        <img src="" alt="" />
-        <figcaption>Les petits nids</figcaption>
+      <figure className="logo">
+        <img src={logo} alt="" />
+        <figcaption className="small-not">Les petits nids</figcaption>
       </figure>
+      <div className="white-painting">
+        <figure className="mother">
+          <img src={mother} alt="" />
+          <figcaption className="welcome">
+            Garde d'enfant à la demande
+          </figcaption>
+        </figure>
+        <p className="account-created">
+          Réservez une place en moins de 60 secondes et obtenez une solution de
+          garde, même pour le lendemain !
+        </p>
+      </div>
+      <div className="button-pass">
+        <p>Passer</p>
+        <button type="button" className="round-button">
+          &gt;
+        </button>
+      </div>
     </div>
   );
 }
