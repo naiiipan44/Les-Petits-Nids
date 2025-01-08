b2820ff26a8c39955aa8d1c3ca2501f228fa9f3b
alicepgrd <pougeardalice@gmail.com>
2025-01-08 10:25:09 +0100

    LoginPage work in progress

--- client/src/pages/RegisterPage.tsx
+++ client/src/pages/RegisterPage.tsx
@@ -1,35 +1,5 @@
-import { useState } from "react";
-import RegisterPageNurseryComponent from "../components/RegisterPageNurseryComponent";
-
 function RegisterPage() {
-  const [activeTab, setActiveTab] = useState("parent");
-  return (
-    <>
-      <h1>Créer un compte</h1>
-      <section className="tab-container">
-        <button
-          type="button"
-          className={`tab-button ${activeTab === "parent" ? "active-tab" : ""}`}
-          onClick={() => setActiveTab("parent")}
-        >
-          En tant que Parent
-        </button>
-        <button
-          type="button"
-          className={`tab-button ${activeTab === "creche" ? "active-tab" : ""}`}
-          onClick={() => setActiveTab("creche")}
-        >
-          En tant que Crèche
-        </button>
-      </section>
-
-      <RegisterPageNurseryComponent />
-      <button className="button primary" type="button">
-        Créer un compte
-      </button>
-      <p>retour</p>
-    </>
-  );
+  return <h1>Ici c'est la register page</h1>;
 }
 
 export default RegisterPage;
