75def6a8564950faf877a7f6236d4a47f45cd416
Clément Praud <clement.praud6@gmail.com>
2025-01-08 09:54:37 +0100

    Create Nursery component

---
+++ client/src/components/Nursery.tsx
@@ -0,0 +1,34 @@
+// function Nursery(
+//   id,
+//   name,
+//   type,
+//   heart,
+//   rate,
+//   location,
+//   openHours,
+//   phoneNumber,
+//   mail,
+// ) {
+//   return (
+//     <>
+//       <article className="nursery-section" key={id}>
+//         <figure>
+//           <img src={name} alt="example of the nursery" />
+//           <figcaption>
+//             {type}
+//             <br />
+//             {heart}
+//           </figcaption>
+//           <p>{heart}</p>
+//           <p>{rate}</p>
+//         </figure>
+//         <h3>{location}</h3>
+//         <p>{openHours}</p>
+//         <p>{phoneNumber}</p>
+//         <p>{mail}</p>
+//       </article>
+//     </>
+//   );
+// }
+
+// export default Nursery;
