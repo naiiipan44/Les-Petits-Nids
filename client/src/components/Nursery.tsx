ebeb617a9f9467fcf0c325217529320a5d290c4e
Clément Praud <clement.praud6@gmail.com>
2025-01-08 10:46:40 +0100

    Replace hard coded nursery cards in SearchPage by a map of Nursery component, move the style of Nursery cards from searchpage.css to nursery.css and create NurseryData interface in type folder

--- client/src/components/Nursery.tsx
+++ client/src/components/Nursery.tsx
@@ -1,34 +1,40 @@
-// function Nursery(
-//   id,
-//   name,
-//   type,
-//   heart,
-//   rate,
-//   location,
-//   openHours,
-//   phoneNumber,
-//   mail,
-// ) {
-//   return (
-//     <>
-//       <article className="nursery-section" key={id}>
-//         <figure>
-//           <img src={name} alt="example of the nursery" />
-//           <figcaption>
-//             {type}
-//             <br />
-//             {heart}
-//           </figcaption>
-//           <p>{heart}</p>
-//           <p>{rate}</p>
-//         </figure>
-//         <h3>{location}</h3>
-//         <p>{openHours}</p>
-//         <p>{phoneNumber}</p>
-//         <p>{mail}</p>
-//       </article>
-//     </>
-//   );
-// }
+// Import style
+import "./nursery.css";
 
-// export default Nursery;
+// Import interface
+import type { DataNursery } from "../types/DataNursery";
+
+function Nursery(dataNursery: DataNursery) {
+  const {
+    id,
+    name,
+    type,
+    image,
+    rate,
+    location,
+    openHours,
+    phoneNumber,
+    mail,
+  } = dataNursery;
+  return (
+    <>
+      <article className="nursery-section" key={id}>
+        <figure>
+          <img src={image} alt="example of the nursery" />
+          <figcaption>
+            {name}
+            <br />
+            {type}
+          </figcaption>
+          <p>{rate}</p>
+        </figure>
+        <h3>{location}</h3>
+        <p>{openHours}</p>
+        <p>{phoneNumber}</p>
+        <p>{mail}</p>
+      </article>
+    </>
+  );
+}
+
+export default Nursery;
