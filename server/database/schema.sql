1d9f45ee4c35ac4f6fb1e3f7a9ddf7a5d01a0030
Naïma PANELLE <168522370+naiiipan44@users.noreply.github.com>
2025-01-10 09:45:04 +0100

    fixed the previous issue by enlarging the varchar to 1000 characters for the ns_description row and also corrected the phone numbers mails of the nursery schools that were missing and added the accurate descriptions

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -21,7 +21,7 @@ CREATE TABLE nursery (
   ns_disabled BOOLEAN DEFAULT FALSE,
   ns_coord_long DECIMAL(10,6) NOT NULL,
   ns_coord_lat DECIMAL(10,6) NOT NULL,
-  ns_description VARCHAR(255),
+  ns_description VARCHAR(1000),
   ns_image VARCHAR(260) NOT NULL,
   ns_price DECIMAL(10,2) NOT NULL
 );
@@ -52,7 +52,7 @@ VALUES
   1,
   "Les Passereaux", 
   "Multi-accueil", 
-  "lespassereaux@creche.org",
+  "creche-les-passereaux@wanadoo.fr",
   "1 RUE CESARIA EVORA", 
   92700, 
   "0147819355", 
@@ -64,7 +64,7 @@ VALUES
   false, 
   2.228716805365273, 
   48.919672135245996,
-  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper.",
+  "La crèche Les Passereaux située au 1 RUE CESARIA EVORA, COLOMBES 92700 est une crèche accueillant les enfants de 10 semaines à 3 ans. Sa capacité d’accueil est de 60. Cet établissement proposent 3 types d’accueil : régulier, occasionnel ou d’urgence. Inscrivez-vous ou connectez-vous pour avoir plus d’informations sur les tarifs et les modalités d’inscription !",
   "https://static.wixstatic.com/media/dc445c_9fb8a975972442eab252760838e90e32~mv2.png/v1/fill/w_980,h_436,q_90/dc445c_9fb8a975972442eab252760838e90e32~mv2.png", 
   1.47
   ),
@@ -73,10 +73,10 @@ VALUES
   2,
   "Babilou Colombes Barbusse",
   "Multi-accueil",
-  "babilou-colombes@creche.org",
+  "contact@babilou.com",
   "102 AVENUE HENRI BARBUSSE",
   92025,
-  "0147819357",
+  "0809103000",
   41,
   1,
   false,
@@ -85,7 +85,8 @@ VALUES
   false,
   2.2526177902864144,
   48.91676085070931,
-  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper.",
+  "Pouvant recevoir jusqu'à 41 enfants, la crèche Babilou Colombes Barbusse accueille vos enfants âgés de 10 semaines à 4 ans du lundi au vendredi de 7h30 à 19h. La crèche est située au 102 avenue Henri Barbusse à Colombes.
+Votre activité professionnelle pouvant vous imposer différentes contraintes horaires, 3 types d'accueil sont possibles afin de s'adapter au mieux à vos besoins : l’accueil régulier, occasionnel et l'accueil d'urgence. En outre, vous bénéficiez d'un service tout compris puisque nous fournissons le lait, les repas et les couches au sein de notre structure. Inscrivez-vous ou connectez-vous pour avoir plus d’informations sur les tarifs et les modalités d’inscription !",
   "https://babilou.fr/sites/default/files/styles/diaporama/public/2022-01/233-babilou-colombes-barbusse-8_0.jpg?h=82f92a78&itok=GDoBekte",
   2.73
   ),
@@ -94,10 +95,10 @@ VALUES
   3,
   "Les Barbusiens",
   "Micro-crèche",
-  "lesbarbusiens@creche.org",
+  "rpe-orves@mairie-colombes.fr",
   "121 AVENUE HENRI BARBUSSE",
   92025,
-  "0146764605",
+  "0185531378",
   12,
   0,
   false,
@@ -106,7 +107,7 @@ VALUES
   false,
   2.252821179703448,
   48.916382503696916,
-  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper.",
+  "Située 121 avenue Henri Barbusse, à proximité de la gare Les Vallées et du square Denis Papin la crèche à Colombes Les Barbusiens accueille chaque jour 10 enfants âgés de 10 semaines à 4 ans dans un cadre de vie sécurisé, adapté au développement des tout-petits et à l’accueil de leur famille. La micro crèche spacieuse (150 m²) à la décoration chaleureuse favorise l'éveil et le bien être des enfants. Inscrivez-vous ou connectez-vous pour avoir plus d’informations sur les tarifs et les modalités d’inscription !",
   "https://www.demain2pieds.com/wp-content/uploads/2018/05/Les-Barbusiens-4.jpg",
   2.50
   ),
@@ -115,7 +116,7 @@ VALUES
   4,
   "Saint Raphaël",
   "Multi-accueil",
-  "saint-raphael@creche.org",
+  "creche@association-saint-raphael.com",
   "15 AVENUE DU BOIS DE VERRIERES",
   92160,
   "0146664605",
@@ -127,7 +128,7 @@ VALUES
   false,
   2.295139018569901,
   48.755042569283084,
-  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper.",
+  "La crèche Saint Raphaël située au 15 AVENUE DU BOIS DE VERRIERES, ANTONY 92160 est une crèche accueillant les enfants de 10 semaines à 3 ans. Sa capacité d’accueil est de 50. Cet établissement proposent 3 types d’accueil : régulier, occasionnel ou d’urgence. Inscrivez-vous ou connectez-vous pour avoir plus d’informations sur les tarifs et les modalités d’inscription !",
   "https://www.acis-asbl.be/wp-content/uploads/2023/07/2gp06473.jpg",
   1.47
   ),
@@ -136,7 +137,7 @@ VALUES
   5,
   "La Comptine - Arc en Ciel",
   "Multi-accueil",
-  "la-comptine-arc@creche.org",
+  "petite_enfance@ville-antony.fr",
   "15 AVENUE FONTAINE MOUTON",
   92160,
   "0146682577",
@@ -148,7 +149,7 @@ VALUES
   true,
   2.289409993720274,
   48.738824991659705,
-  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper."
+  "La crèche La Comptine – Arc en Ciel située au 15 AVENUE FONTAINE MOUTON, ANTONY 92160 est une crèche accueillant les enfants de 10 semaines à 3 ans. Sa capacité d’accueil est de 55. Cet établissement proposent 3 types d’accueil : régulier, occasionnel ou d’urgence. Inscrivez-vous ou connectez-vous pour avoir plus d’informations sur les tarifs et les modalités d’inscription !",
   "https://www.ville-antony.fr/images/Petite-Enfance/photos_multi-accueils/la_comptine/int/farandole/farandole_int_1_500.jpg",
   1.73
   );
