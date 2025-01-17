494f57c6d70828bdf160c5cfd4c4f15bf17bdf1e
LucasBbe <lucas.boube@gmail.com>
2025-01-17 12:21:22 +0100

    add a condition in app via the url path for adding the navbar or not if in the / route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,16 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import NavBar from "./components/NavBar";
 import "./style/globals.css";
 
 function App() {
+  const location = useLocation();
+  const isLandingPage = location.pathname === "/";
+
   return (
     <>
       <Outlet />
-      <NavBar />
+      {isLandingPage ? "" : <NavBar />}
       <ToastContainer />
     </>
   );
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -7,7 +7,6 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 // Import the main app component
 import App from "./App";
-import Favorites from "./pages/Favorites";
 import LandingPage from "./pages/LandingPage";
 import LoginAndRegisterPage from "./pages/LoginAndRegisterPage";
 import NurseryPage from "./pages/NurseryPage";
@@ -51,10 +50,6 @@ const router = createBrowserRouter([
       {
         path: "loginandregister",
         element: <LoginAndRegisterPage />,
-      },
-      {
-        path: "favorites",
-        element: <Favorites />,
       },
     ],
   },
