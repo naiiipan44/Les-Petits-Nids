c85f06eee100eab929a5cc37999fa0548e0cc575
Clément Praud <clement.praud6@gmail.com>
2025-01-22 15:17:29 +0100

    Add smoke test on route GET /api/userLogin

--- server/tests/user/user.test.ts
+++ server/tests/user/user.test.ts
@@ -1,10 +1,30 @@
+// Import the supertest library for making HTTP requests
 import supertest from "supertest";
+
+// Import the Express application
 import app from "../../src/app";
 
+// Import databaseClient
+import databaseClient from "../../database/client";
+
+import type { Rows } from "../../database/client";
+
+// Restore all mocked functions after each test
+afterEach(() => {
+  jest.restoreAllMocks();
+});
+
 describe("GET /api/userLogin", () => {
-  test("Should return status 201", async () => {
+  it("Should return status 201", async () => {
+    const rows = [] as Rows;
+
+    jest
+      .spyOn(databaseClient, "query")
+      .mockImplementation(async () => [rows, []]);
+
     const response = await supertest(app).get("/api/userLogin");
 
-    expect(response.status).toBe(201);
+    expect(response.status).toBe(200);
+    expect(response.body).toStrictEqual(rows);
   });
 });
