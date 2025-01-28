2568c9de4a1129540a487b7d48f2011e3c12cbbb
alicepgrd <pougeardalice@gmail.com>
2025-01-28 15:11:12 +0100

    text nav bar added

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -8,31 +8,40 @@ function NavBar() {
         <img src="/logo.svg" alt="Ceci est le logo des Petits Nids" />
         <figcaption className="small-not-navbar">Les Petits Nids</figcaption>
       </figure>
-      <NavLink to="favorites">
-        <button type="button" className="nav-buttons">
-          <img src="/blueheart.svg" alt="link to the favorite page" />
-        </button>
-      </NavLink>
-      <NavLink to="search">
-        <button type="button" className="nav-buttons">
-          <img src="/search.svg" alt="link to the search page" />
-        </button>
-      </NavLink>
-      <NavLink to="loginandregister">
-        <button type="button" className="nav-buttons">
-          <img src="/user.svg" alt="link to the profil of the user" />
-        </button>
-      </NavLink>
-      <NavLink to="parentsbookings">
-        <button type="button" className="nav-buttons">
-          <img src="/books.svg" alt="link to the book page" />
-        </button>
-      </NavLink>
-      <NavLink to="map">
-        <button type="button" className="nav-buttons">
-          <img src="/map.svg" alt="link to the map page" />
-        </button>
-      </NavLink>
+      <section className="button-navbar-text">
+        <h1 className="menu-navbar">Menu</h1>
+        <NavLink to="favorites">
+          <button type="button" className="nav-buttons">
+            <img src="/blueheart.svg" alt="link to the favorite page" />
+            <h2 className="title-navbar">Favoris</h2>
+          </button>
+        </NavLink>
+        <NavLink to="search">
+          <button type="button" className="nav-buttons">
+            <img src="/search.svg" alt="link to the search page" />
+            <h2 className="title-navbar">Page de recherche</h2>
+          </button>
+        </NavLink>
+        <NavLink to="loginandregister">
+          <button type="button" className="nav-buttons">
+            <img src="/user.svg" alt="link to the profil of the user" />
+            <h2 className="title-navbar">Profil</h2>
+          </button>
+        </NavLink>
+        <NavLink to="map">
+          <button type="button" className="nav-buttons">
+            <img src="/map.svg" alt="link to the map page" />
+            <h2 className="title-navbar">Map</h2>
+          </button>
+        </NavLink>
+        <h1 className="menu-navbar">Suivi</h1>
+        <NavLink to="parentsbookings">
+          <button type="button" className="nav-buttons">
+            <img src="/books.svg" alt="link to the book page" />
+            <h2 className="title-navbar">Reservations</h2>
+          </button>
+        </NavLink>
+      </section>
     </nav>
   );
 }
