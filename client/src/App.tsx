3e38fdf97220aba58c4bf96fd577dae174a76280
alicepgrd <pougeardalice@gmail.com>
2025-01-29 09:10:14 +0100

    navbar desktop done and footer done

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
 import { Outlet, useLocation } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
+import Footer from "./components/Footer";
 import NavBar from "./components/NavBar";
 import "./style/globals.css";
 
@@ -16,6 +17,7 @@ function App() {
         {isLandingPage ? "" : <NavBar />}
         <ToastContainer />
       </section>
+      <Footer />
     </section>
   );
 }
