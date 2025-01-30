37c3dfef5e637494c88a655004e2af783b3c3356
LucasBbe <lucas.boube@gmail.com>
2025-01-30 17:29:10 +0100

    Media queries added to login and register pages for PC and tablet devices

--- client/src/pages/LoginAndRegisterPage.tsx
+++ client/src/pages/LoginAndRegisterPage.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import "./LoginAndRegisterPage.css";
 
-import { Link } from "react-router-dom";
 import LoginPageComponent from "../components/LoginPageComponent";
 import RegisterPageComponent from "../components/RegisterPageComponent";
 
@@ -10,76 +9,80 @@ function LoginAndRegisterPage() {
   const [registration, setRegistration] = useState<boolean>(false);
 
   return (
-    <>
+    <section className="login-register-background">
       <section className="login-page-container">
-        <figure className="logo">
-          <img src="/logo.svg" alt="logo" />
-        </figure>
-        {registration ? (
-          <h1 className="login-title">Créer un compte</h1>
-        ) : (
-          <h1 className="login-title">Se connecter</h1>
-        )}
-        <section className="user-type-toggle">
-          <input
-            type="radio"
-            id="user-type-parent"
-            name="user-type"
-            value="parent"
-          />
-          <label className="radio-button" htmlFor="user-type-parent">
-            {" "}
-            En tant que parent
-          </label>
+        <section className="section-form-login">
+          <figure className="logo">
+            <img src="/bluelogo.png" alt="logo" />
+          </figure>
+          {registration ? (
+            <h1 className="login-title">Créer un compte</h1>
+          ) : (
+            <h1 className="login-title">Se connecter</h1>
+          )}
+          <section className="user-type-toggle">
+            <input
+              type="radio"
+              id="user-type-parent"
+              name="user-type"
+              value="parent"
+            />
+            <label className="radio-button" htmlFor="user-type-parent">
+              {" "}
+              En tant que parent
+            </label>
 
-          <input
-            type="radio"
-            id="user-type-nursery"
-            name="user-type"
-            value="nursery"
-          />
-          <label className="radio-button" htmlFor="user-type-nursery">
-            En tant que crèche
-          </label>
+            <input
+              type="radio"
+              id="user-type-nursery"
+              name="user-type"
+              value="nursery"
+            />
+            <label className="radio-button" htmlFor="user-type-nursery">
+              En tant que crèche
+            </label>
+          </section>
+
+          {registration ? (
+            <RegisterPageComponent
+              isParent={isParent}
+              setIsParent={setIsParent}
+            />
+          ) : (
+            <LoginPageComponent />
+          )}
+          {registration ? (
+            <button
+              type="button"
+              className="account-button"
+              onClick={() => setRegistration(!registration)}
+            >
+              Vous avez déjà un compte ? Se connecter
+            </button>
+          ) : (
+            <button
+              type="button"
+              className="account-button"
+              onClick={() => setRegistration(!registration)}
+            >
+              Pas encore de compte ? Inscris-toi
+            </button>
+          )}
+          {!registration && <p className="links">Mot de passe oublié</p>}
         </section>
-
-        {registration ? (
-          <RegisterPageComponent
-            isParent={isParent}
-            setIsParent={setIsParent}
+        <section className="section-image-login">
+          <img
+            src="LoginAndRegister-Image.png"
+            alt="Desssin d'enfants dans une crèche"
           />
-        ) : (
-          <LoginPageComponent />
-        )}
-        {registration ? (
-          <button
-            type="button"
-            className="account-button"
-            onClick={() => setRegistration(!registration)}
-          >
-            Vous avez déjà un compte ? Se connecter
-          </button>
-        ) : (
-          <button
-            type="button"
-            className="account-button"
-            onClick={() => setRegistration(!registration)}
-          >
-            Pas encore de compte ? Inscris-toi
-          </button>
-        )}
-        {!registration && (
-          <Link to="" className="links">
-            Mot de passe oublié
-          </Link>
-        )}
+        </section>
       </section>
       <section className="button-return">
         <button className="back-button" type="button">
           <span className="arrow" />
         </button>
       </section>
-    </>
+    </section>
   );
 }
 
