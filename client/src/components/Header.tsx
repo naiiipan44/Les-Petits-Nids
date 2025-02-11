ee5d5ac0c591ba6a2f909fd92a5decd100e01b5c
LucasBbe <lucas.boube@gmail.com>
2025-02-11 14:04:42 +0100

    fetched user and added his firstname and lastname in the header

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,10 +1,43 @@
+import { useEffect, useState } from "react";
+import type { User } from "../types/AuthContext";
 import "./Header.css";
 
 function Header() {
+  const [user, setUser] = useState<User>(null);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await fetch(
+          `${import.meta.env.VITE_API_URL}/api/user/me`,
+          {
+            method: "GET",
+            credentials: "include",
+          },
+        );
+
+        if (response.ok) {
+          const data = await response.json();
+          setUser(data.user || null);
+        } else {
+          setUser(null);
+        }
+      } catch (error) {
+        console.error(
+          "Erreur lors de la récupération de l'utilisateur :",
+          error,
+        );
+        setUser(null);
+      }
+    };
+
+    fetchUser();
+  }, []);
+
   return (
     <header className="profil-header">
       <img src="/profile.svg" alt="profile-picture" />
-      <figcaption>Jean Charles</figcaption>
+      <figcaption>{`${user?.first_name} ${user?.last_name}`}</figcaption>
       <img src="/chevron.png" alt="chevron pour menu déroulant" />
     </header>
   );
