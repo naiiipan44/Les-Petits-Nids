48f48ad6005e29b908e2c98c954e80844fd19a3c
Clément Praud <clement.praud6@gmail.com>
2025-02-11 12:29:35 +0100

    Update client side content on profile page with user infos fetched with cookie user id and user table on bdd

--- client/src/components/ParentProfile.tsx
+++ client/src/components/ParentProfile.tsx
@@ -1,53 +1,78 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "../pages/ProfilePage.css";
 import type { ParentProfileProps } from "../types/ParentProfile";
+import type { UserParent } from "../types/UserParent";
 
 function ParentProfile({ setDisplay, setIsVisible }: ParentProfileProps) {
+  const [userId, setUserId] = useState(0);
+
+  const [parent, setParent] = useState<UserParent | null>(null);
+
   useEffect(() => {
-    fetch(`${import.meta.env.VITE_API_URL}/api/user?userId=1`)
+    fetch(`${import.meta.env.VITE_API_URL}/api/user/me`, {
+      credentials: "include",
+    })
       .then((response) => response.json())
-      .then((parent) => console.warn(parent));
-  }, []);
+      .then((user) => setUserId(user.user.id));
+
+    if (userId) {
+      fetch(`${import.meta.env.VITE_API_URL}/api/user?userId=${userId}`)
+        .then((response) => response.json())
+        .then((user) => {
+          if (user.length) {
+            setParent(user[0]);
+          }
+        });
+    }
+  }, [userId]);
 
   return (
     <section className="profile-page">
-      <figure className="profile">
-        <img src="/profile.svg" alt="portrait de profil" />
-        <figcaption>Ed Cannon</figcaption>
-        <h2>Papa Poule</h2>
-      </figure>
-      <h3>Mettez toutes les chances de votre côté</h3>
-      <p>Un profil complet est nécessaire pour un accueil en crèche</p>
-      <button
-        type="button"
-        className="child-button"
-        onClick={() => {
-          setDisplay(false);
-          setIsVisible("children");
-        }}
-      >
-        Dossier Enfants
-      </button>
-      <button
-        type="button"
-        className="parents-button"
-        onClick={() => {
-          setDisplay(false);
-          setIsVisible("parent");
-        }}
-      >
-        Dossier Parents
-      </button>
-      <button
-        type="button"
-        className="reservations-button"
-        onClick={() => {
-          setDisplay(false);
-          setIsVisible("booking");
-        }}
-      >
-        Mes reservations
-      </button>
+      {userId > 0 && parent ? (
+        <>
+          <figure className="profile">
+            <img src="/profile.svg" alt="portrait de profil" />
+            <figcaption>
+              {parent.first_name} {parent.last_name}
+            </figcaption>
+            <h2>{parent.role}</h2>
+          </figure>
+          <h3>Mettez toutes les chances de votre côté</h3>
+          <p>Un profil complet est nécessaire pour un accueil en crèche</p>
+          <button
+            type="button"
+            className="child-button"
+            onClick={() => {
+              setDisplay(false);
+              setIsVisible("children");
+            }}
+          >
+            Dossier Enfants
+          </button>
+          <button
+            type="button"
+            className="parents-button"
+            onClick={() => {
+              setDisplay(false);
+              setIsVisible("parent");
+            }}
+          >
+            Dossier Parents
+          </button>
+          <button
+            type="button"
+            className="reservations-button"
+            onClick={() => {
+              setDisplay(false);
+              setIsVisible("booking");
+            }}
+          >
+            Mes reservations
+          </button>
+        </>
+      ) : (
+        <p>Vous devez être connecté pour accéder à cette page</p>
+      )}
     </section>
   );
 }
